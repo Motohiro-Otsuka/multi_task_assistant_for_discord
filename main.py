e0e451b498e0bedf146c752492add8522c99c6d8
Motohiro-Otsuka <107312091+Motohiro-Otsuka@users.noreply.github.com>
2023-12-18 11:37:30 +0000

    ボットが起動するように修正

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ intents.members = True
 intents.message_content = True  # メッセージに関するデバッグ情報を有効化
 client = commands.Bot(command_prefix="/", intents=intents)
 
+
 @client.event
 async def on_ready():
-    print('ログインしました')
+  print('ログインしました')
 
 
 @client.event
-async def on_message(message):#メッセージをなにかしら受け取ったときの処理
-    if type(message.channel) is discord.Thread:#スレッドでメッセージを受け取った時
-        if message.author == client.user or message.author.bot:#メッセージの送り主がbotの時は処理しない
-            pass
-        elif message.channel.id not in allow_recoed_thread_id:#録画を許可しないスレッドは処理しない
-            pass
-        else:#ここに保存コマンドを書く
-            pass
-    #それ以外はコマンド実行
+async def on_message(message):  #メッセージをなにかしら受け取ったときの処理
+  if type(message.channel) is discord.Thread:  #スレッドでメッセージを受け取った時
+    if message.author == client.user or message.author.bot:  #メッセージの送り主がbotの時は処理しない
+      pass
+    elif message.channel.id not in allow_recoed_thread_id:  #録画を許可しないスレッドは処理しない
+      pass
+    else:  #ここに保存コマンドを書く
+      pass
+  #それ以外はコマンド実行
+  if message.author == client.user or message.author.bot:
+    pass
+  else:
     await message.channel.send('メッセージを受信しました。')
+
+
 client.run(os.environ["discord_api_key"])
